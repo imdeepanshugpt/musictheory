e0f2fdfd2cce4c023b5019f1b338488af21411a7
imdeepanshugpt <imdeepanshugpt@gmail.com>
2023-07-29 22:18:53 +0530

    fixed sw issue

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import * as serviceWorker from './service-worker';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -13,7 +11,6 @@ root.render(
   </React.StrictMode>
 );
 
-serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
